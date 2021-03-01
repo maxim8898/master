3908100493ca149ef607959ef924abd5d9373256
Maksim_Matuzka <maksim_matuzka@epam.com>
2021-03-01 11:53:50 +0300

    Added mocked components.

--- app/src/index.js
+++ app/src/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import DevCorner from './components/DevCorner';
 
 ReactDOM.render(
   <React.StrictMode>
+    <DevCorner />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
