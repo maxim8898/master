eb18e6bde63a18ca831e650d6c90fc98ba6a3c36
Aliaksei Hushcha <aliaksei_hushcha@epam.com>
2021-04-11 12:18:21 +0300

    Init mongo 'snapshots' database

--- api/main.py
+++ api/main.py
@@ -60,7 +60,3 @@ def get_data():
 
     return {"collection": snap_list.dict()}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
---
+++ mongo-sensors-init.js
@@ -0,0 +1,11 @@
+db.sensors.insertOne(
+    {
+        "left": 1,
+        "left_center": 1,
+        "right_center": 1,
+        "right": 1,
+        "speed": 1,
+        "temperature": 1,
+        "angle": 1
+    }
+);
