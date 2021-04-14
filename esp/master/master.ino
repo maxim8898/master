576ef32a82d3afe07a5090f7d85f13e316340aca
Maksim_Matuzka <Maksim_Matuzka@epam.com>
2021-04-14 17:06:43 +0300

    master.ino was added.

--- esp/master/master.ino
+++ esp/master/master.ino
@@ -1,9 +1,177 @@
-void setup() {
-  // put your setup code here, to run once:
+// Import required libraries
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <ArduinoJson.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "Adafruit_VL53L0X.h"
 
+// Replace with your network credentials
+const char* ssid = "HUAWEI-66nK";
+const char* password = "1168001754";
+const char* serverName = "http://example.com/";
+
+bool ledState = 0;
+int currentDistanse = 0;
+const int ledPin = 2;
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+AsyncWebSocket ws("/sensors");
+HTTPClient http;
+
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
+void notifyClients() {
+  ws.textAll(String(ledState));
+}
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    data[len] = 0;
+    if (strcmp((char*)data, "toggle") == 0) {
+      ledState = !ledState;
+      notifyClients();
+    }
+  }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
+             void *arg, uint8_t *data, size_t len) {
+    switch (type) {
+      case WS_EVT_CONNECT:
+        Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+        break;
+      case WS_EVT_DISCONNECT:
+        Serial.printf("WebSocket client #%u disconnected\n", client->id());
+        break;
+      case WS_EVT_DATA:
+        handleWebSocketMessage(arg, data, len);
+        break;
+      case WS_EVT_PONG:
+      case WS_EVT_ERROR:
+        break;
+  }
+}
+
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+}
+
+String processor(const String& var){
+  Serial.println(var);
+  if(var == "STATE"){
+    if (ledState){
+      return "ON";
+    }
+    else{
+      return "OFF";
+    }
+  }
+}
+
+String httpGETRequest(const char* serverName) {
+  HTTPClient http;
+    
+  // Your IP address with path or Domain name with URL path 
+  http.begin(serverName);
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+  
+  String payload = "{}"; 
+  
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+  return payload;
+}
+
+String httpPOSTRequest(String json, char* serverName, char* path) {
+  HTTPClient http;
+  http.begin(strcat(serverName, path));
+  int httpResponseCode = http.POST(json);
+  
+  String payload = "{}"; 
+  
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+  return payload;
+}
+
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+
+  Serial.println("Adafruit VL53L0X test");
+  if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }
+
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  // Print ESP Local IP Address
+  Serial.println(WiFi.localIP());
+
+  initWebSocket();
+
+  // Start server
+  server.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+//   DynamicJsonDocument doc(2048);
+//   doc["hello"] = "world";
+//   String json;
+//   serializeJson(doc, json);
+
+//   Serial.print(httpPOSTRequest(json, "https://postman-echo.com/", "post"));
+
+  VL53L0X_RangingMeasurementData_t measure;
+  Serial.println("Reading a measurement... ");
+  lox.rangingTest(&measure, false);
+
+  if (measure.RangeStatus != 4) {
+    ws.textAll(String(measure.RangeMilliMeter));
+  }
+  else {
+    ws.textAll(String(" out of range "));
+  }
+
+  ws.cleanupClients(); // uint16_t maxClients < WS_CONNECTED clients.
+  digitalWrite(ledPin, ledState);
+
+  delay(100);
 }
